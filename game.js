d6bd0bc2d703d10c9956073f31a91873525cd039
scottpschaffer <scottpschaffer@yahoo.com>
2016-11-24 10:11:09 -0600

    Successfully merged code from Work PC with Laptop code

--- game.js
+++ game.js
@@ -13,11 +13,26 @@
 
     // Residing place for our Canvas' context
     var ctx = null;
+    // Array of bricks
     var brix1 = [];
+    // Array of words that will be compared against to determine what has changed
     var masterWords = [];
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    // Array of bad words to compare against
+    var badWords = [];
+    // Array temporarily acting as dictionary in place of DB
+    var dictList1 = [{name: "CAB", definition: "Going somewhere in someone else's car"},
+                    {name: "BAC", definition: "The side you sleep on"},
+                    {name: "CACA", definition: "The poop"}];
+    // Letters of Alaphabet usable
+    var letters = ["A", "B", "C"];
+    //, "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var colors = ["red", "orange", "green", "purple", "blue"]
-    var xx = true;
+    // Used to allow game to replay
+    var again = true;
+    // Area where HTML where definition is displayed
+    var defText;
+    
+
     var Game = {
         // Setup configuration
         canvas: document.getElementById('canvas'),
@@ -37,10 +52,11 @@
                 //setTimeout(Ctrl.init, 100000);
                 
             }
+            defText = document.getElementById('def');
         },
 
         init: function(){
-            Paddle.init();
+            FallingBrick.init();
         },
 
         runGame: function() {
@@ -52,13 +68,13 @@
         },
 
         restartGame: function(){
-            xx = true;
+            again = true;
         },
 
         animate: function(){
             Game.play = requestAnimFrame(Game.animate);
 
-            if (xx){
+            if (again){
                 Game.draw();
             }
            
@@ -66,7 +82,7 @@
 
         draw: function(){
             ctx.clearRect(0, 0, this.width, this.height);
-            Paddle.draw();
+            FallingBrick.draw();
             Bricks.draw();
         }
     };
@@ -108,10 +124,9 @@
         }
     };
 
-    var Paddle = {
+    var FallingBrick = {
         w: 50,
         h: 20,
-        r: 9,
         
         init: function(){
             this.x = 100;
@@ -152,8 +167,8 @@
 
                     Game.canvas.addEventListener('click', Game.restartGame, false);
                     Screen.gameover();
-                    xx = false;
-                    console.log(xx);
+                    again = false;
+                    console.log(again);
                     return;
                 }
                 else
@@ -163,7 +178,7 @@
                 
             }
             
-        },
+        }
 
     }
 
@@ -198,38 +213,69 @@
             return false;
         },
 
+        // This needs to be separate from collide because brick already in Brix1 and has to skip itself
+        collide2: function(x2, y2, n2){
+            for (var j = 0; j < brix1.length; j++)
+            {
+                if (j != n2)
+                {
+                    if ((((x2 + 50) > brix1[j].x) && ((x2+50) <=(brix1[j].x + 50)) || (x2 >= brix1[j].x) && (x2 < (brix1[j].x + 50))) && (((y2 + 20) >= brix1[j].y) && ((y2 + 20) <= (brix1[j].y + 20))))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
+
         process: function(){
             var words = this.getListOfWords();
+            this.checkIfWords(words);
+            this.moveBricksDown();
         },    
 
         getListOfWords: function(){
             var temp1 = [];
             var temp2 = [];
+            
+            // From 530 (top) to 210 (limit of bricks)
             for (var y = 530; y > 210; y--)
             {
                 temp2 = [];
+
+                // 410 is width of canvas minus 50 (width of brick) = 360
                 for (var x = 0; x < 360; x++)
                 {
-                    
+                    // Go through every brick in list
                     for (var n = 0; n < brix1.length; n++)
                     {
-
+                        // If x/y corner of brick matches anything in list of bricks
                         if ((x == brix1[n].x) && (y == brix1[n].y))
                         {   
-                            //console.log("The letter is: " + brix1[n].l);
-                            temp2.push(brix1[n].l);
+                            // Add the letter and brick # to list
+                            temp2.push({letters: brix1[n].l, number: n});
                         }
                     }
-                    //console.log(temp2.toString());
                     
                 }
-                temp1[y] = temp2.toString();
-                console.log("The Word is " + temp1[y]);
+
+                var temp3= [];
+                var temp4 = [];
+                for (var z = 0; z < temp2.length; z++)
+                {
+                    // Have to use this arrays because can't join on temp2[z].letters
+                    temp3.push(temp2[z].letters);
+                    temp4.push(temp2[z].number);
+                }
+                // Return list of words and the block numbers that make up each word (used for deletions)
+                temp1.push({word: (temp3.join("")), numb: temp4});
             }
             return temp1;
         },
 
         checkIfWords: function(cList){
+            var coords = [];
+            coords.sort;
             if (masterWords.length == 0)
             {
                 masterWords = cList;
@@ -238,14 +284,113 @@
             {
                 for (var x = 0; x < cList.length; x++)
                 {
-                    if (cList[x] != masterWords[x])
+                    // If word in array of words doesn't match its counterpart in masterlist then change happened
+                    if (cList[x].word != masterWords[x].word)
                     {
-                        var result = findWord(cList[x]);
-                        x
+                        coords = this.findSubWord(cList[x].word);
+                        // remove Bricks
+                        if (coords.length > 1)
+                        {
+                            var werd = cList[x].numb;
+
+                            var removePart1 = werd.splice(coords[0], (coords[1] - coords[0]) + 1);
+                            removePart1.sort(function(a, b){return b-a});
+
+                            for (var u = 0; u < removePart1.length; u++)
+                            {
+                                brix1.splice(removePart1[u], 1);
+                            }
+                        }
+                        masterWords[x] = cList[x];
                     }
                 }
             }
-        }            
+        },
+
+        findSubWord: function(daWord){
+            var w;
+            var nums = [];
+            for (var x = 0; x < (daWord.length - 2); x++)
+            {
+                for (var y = (daWord.length - 1); y > x; y--)
+                {
+                    w = daWord.substring(x, y+1);
+                    // If not in badWords array
+                    if (!(this.isBadWord(w)))
+                    {
+                        // Check if word in Dictionary
+                        var z = this.isInDictionary(w);
+                        // Dictionary returns "-1" string if not in Dictionary
+                        if (z != "-1")
+                        {
+                            // Return Start and Stop section of word that is in Dictionary
+                            nums.push(x);
+                            nums.push(y);
+                            return nums;
+                        }
+                        else
+                        {
+                            // If not in Dictionary, then put in Array of known bad words
+                            badWords.push(w);
+                        }
+                    }
+                }
+            }
+            return nums;
+        },
+
+        isBadWord: function(daWord)
+        {
+            for (var x = 0; x < badWords.length; x++)
+            {
+                if (daWord == badWords[x])
+                {
+                    return true;
+                }
+            }
+            return false;
+        },
+
+        isInDictionary: function(daWord)
+        {
+            for (var x = 0; x < dictList1.length; x++)
+            {
+                if (daWord == dictList1[x].name)
+                {
+                    var declareDef =  dictList1[x].name + " - Definition: " + dictList1[x].definition;
+                    defText.textContent = declareDef;
+                    this.xhrTest();
+                    return declareDef;
+                }
+            }
+            return "-1";
+        },
+
+        moveBricksDown: function(){
+            for (var n = 0; n < brix1.length; n++)
+            {
+                // If x/y corner of brick matches anything in Brix1 array
+                while ((brix1[n].y < 530) && !(Bricks.collide2(brix1[n].x, brix1[n].y, n)))
+                {
+                    brix1[n].y += 1;
+                }
+            }
+        },
+
+        reqListener: function(response) {
+            //debugger
+            console.log(response.srcElement.responseText);
+            //getElementsByTagName("dt")[0].nodeValue);
+        },
+
+        xhrTest: function()
+        {
+            var oReq = new XMLHttpRequest();
+            oReq.addEventListener("load", this.reqListener);
+            oReq.open("GET", "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/spoon?key=16d591fc-9c27-4304-8057-5faeb1d1da35");
+            oReq.send();
+            console.log("oReq: " + oReq + " reqListener: ");
+        }
     };
 
     var Ctrl = {
@@ -253,7 +398,7 @@
             // Browser based events
             window.addEventListener('keydown', this.keyDown, true);
             window.addEventListener('keyup', this.keyUp, true);
-            window.addEventListener('mousemove', this.movePaddle, true);
+            window.addEventListener('mousemove', this.moveFallingBrick, true);
         },
 
         keyDown: function(event) {
@@ -282,17 +427,17 @@
             }
         },
 
-        movePaddle: function(event) {
+        moveFallingBrick: function(event) {
             var mouseX = event.pageX;
             //console.log(mouseX);
             var canvasX = Game.canvas.offsetLeft;
-            var paddleMid = Paddle.w;
-            //console.log ("canvasX: " + canvasX + " Paddle.w: " + Paddle.w);
-            if (mouseX > canvasX + Paddle.w &&
+            var FallingBrickMid = FallingBrick.w;
+            //console.log ("canvasX: " + canvasX + " FallingBrick.w: " + FallingBrick.w);
+            if (mouseX > canvasX + FallingBrick.w &&
                 mouseX < canvasX + Game.width) {
                 var newX = mouseX - canvasX;
-                newX -= paddleMid;
-                Paddle.x = newX;
+                newX -= FallingBrickMid;
+                FallingBrick.x = newX;
             }
         }
     };
